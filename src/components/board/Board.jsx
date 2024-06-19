9cdb0448def1ed7c67d60ead5c17baf2b8a97201
shubham Rawat <rawatshubam269@gmail.com>
2024-06-19 09:43:37 +0530

    UNDO & REDO Feature Added

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -7,6 +7,8 @@ const Board = () => {
   const dispatch = useDispatch();
   const canvasRef = useRef(null);
   const shouldDraw = useRef(false);
+  const drawHistory = useRef([]);
+  const historyPointer = useRef(0);
   const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu);
   const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
 
@@ -22,6 +24,20 @@ const Board = () => {
       anchor.href = URL;
       anchor.download = "sketch.jpg";
       anchor.click();
+    } else if (
+      actionMenuItem === MENU_ITEMS.UNDO ||
+      actionMenuItem === MENU_ITEMS.REDO
+    ) {
+      if (historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO)
+        historyPointer.current -= 1;
+      if (
+        historyPointer.current < drawHistory.current.length - 1 &&
+        actionMenuItem === MENU_ITEMS.REDO
+      )
+        historyPointer.current += 1;
+      const imageData = drawHistory.current[historyPointer.current];
+      // console.log(imageData);
+      context.putImageData(imageData, 0, 0);
     }
     dispatch(actionItemClick(null));
   }, [actionMenuItem]);
@@ -69,6 +85,10 @@ const Board = () => {
 
     const handleMouseUp = (e) => {
       shouldDraw.current = false;
+
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      drawHistory.current.push(imageData);
+      historyPointer.current = drawHistory.current.length - 1;
     };
 
     canvas.addEventListener("mousedown", handleMouseDown);
